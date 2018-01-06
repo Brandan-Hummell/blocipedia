6dfe9e644111bf19a18308e873280e2ccb5bab32
Brandan <bh2df@virginia.edu>
2018-01-05 22:24:24 -0500

    Checkpoint 2: Users can sign up and there will be a confirmation email. Uses SMTP settings in the devise mailer

---
+++ config/application.rb
@@ -0,0 +1,26 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Blocipedia
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+    ActionMailer::Base.smtp_settings = {
+      user_name:      ENV['SENDMAIL_USERNAME'],
+      password:       ENV['SENDMAIL_PASSWORD'],
+      domain:         ENV['MAIL_HOST'],
+      address:       'smtp.gmail.com',
+      port:          '587',
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  root to: "home#index"
+end
