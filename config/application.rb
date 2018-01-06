d0893fa835f443c95c282a184440146d20a810c0
Brandan <bh2df@virginia.edu>
2018-01-05 22:31:55 -0500

    Checkpoint 3: Sign in and out, edit password

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+# Checkpoint 3 marker
 Bundler.require(*Rails.groups)
 
 module Blocipedia
